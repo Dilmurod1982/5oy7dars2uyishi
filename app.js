d22e698ed8ca1c4bd6a998006405a525b9a61984
cipher <dilsurbet@hmail.com>
2024-05-31 20:34:53 +0500

    1

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-const form = document.querySelector("form");
 const newTodo = document.getElementById("new-todo");
 let ul = document.getElementById("todo-list");
 let temp = document.querySelector("template");
 
 newTodo.addEventListener("keypress", (e) => {
-  let clon = temp.content.cloneNode(true);
-  let textP = document.querySelector("#p-text");
-  //   if (e.key == "Enter") {
-
-  textP.textContent = form["new-Todo"].value;
-  ul.appendChild(clon);
-  console.log(newTodo.value);
-  console.log(form["new-todo"].value);
-  //   }
+  if (e.key == "Enter") {
+    let clon = temp.content.cloneNode(true);
+    let textP = clon.querySelector("#p-text");
+    textP.textContent = newTodo.value;
+    ul.appendChild(clon);
+    newTodo.value = "";
+  }
 });
